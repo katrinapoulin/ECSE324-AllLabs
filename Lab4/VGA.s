cd04439ac53d59808852c111913491b7ce7d347c
katrinapoulin <katrina.poulin@mail.mcgill.ca>
2020-04-14 13:14:10 -0400

    done and commented

--- Lab4/VGA.s
+++ Lab4/VGA.s
@@ -4,18 +4,18 @@ _start:
 
 //MAIN
 main:
-	LDR R0, =0xFF200050
-	LDR R12, =0x140
-	LDR R1, [R0]
-	CMP R1, #0
+	LDR R0, =0xFF200050		//base address of pushbuttons
+	LDR R12, =0x140			//store 320 in R12
+	LDR R1, [R0]			//get data from pushbuttons
+	CMP R1, #0				//if no pushbutton is pressed loop again
 	BEQ main
-	CMP R1, #1
+	CMP R1, #1				//if PB0 pressed do test_byte
 	BLEQ test_byte
-	CMP R1, #2
+	CMP R1, #2				//if PB1 is pressed do test_char
 	BLEQ test_char
-	CMP R1, #4
+	CMP R1, #4				//if PB2 is pressed do test_pixel
 	BLEQ test_pixel
-	CMP R1, #8
+	CMP R1, #8				//if PB3 is pressed do clear_buffs
 	BLEQ clear_buffs
 	B main
 
@@ -27,45 +27,56 @@ clear_buffs:
 
 test_byte:
 	PUSH {R3-R12, LR}
+	//initialize x, y, byte
 	MOV R0, #0	//x=0
 	MOV R1, #0	//y=0
 	MOV R2, #0	//c=0
 	B loop_test_byte
 loop_test_byte:
-	CMP R1, #60
+	CMP R1, #60				//check if y is done (buffer is full)
 	BGE done
-	CMP R0, #80
-	ADDGE R1, R1, #1
+	CMP R0, #80				//check if x is done (if so increment y)
+	ADDGE R1, R1, #1		//change row
 	MOVGE R0, #0
-	BGE loop_test_byte
-	BL VGA_write_byte_ASM
-	CMP R2, #0xFF
+	BGE loop_test_byte		//restart with fresh row
+	BL VGA_write_byte_ASM	//write byte
+	CMP R2, #0xFF			//check if R2 reached FF, if so we reset it to 00
 	MOVEQ R2, #0
-	ADDNE R2, R2, #1
-	ADD R0, R0, #2
+	ADDNE R2, R2, #1		//if not reached, increment
+	ADD R0, R0, #2			//increment x (by 2 because inside the write byte subroutine we already increment by 1)
 	B loop_test_byte
 
 /*WRITE CHAR BYTE
-	//R0:x position			R7:
-	//R1:y position 		R8:
-	//R2:char				R9:
-	//R3:ascii table chars				R10:
-	//R4:stored char			R11:
-	//R5:			R12: 
+	//R0:x position					R7:
+	//R1:y position 				R8:
+	//R2:char						R9:
+	//R3:ascii table chars			R10:
+	//R4:stored char				R11:
+	//R5:							R12: 
 	//R6:
 	*/
 VGA_write_byte_ASM:
 	PUSH {R3-R12, LR}
-	LDR R3, =ascii_chars	
-	MOV R4, R2				
-	LSR R2, R4, #4			
-	LDRB R2, [R3, R2]		
-	BL	VGA_write_char_ASM	
-	ADD R0, R0, #1			
-	AND R2, R4, #15			
-	LDRB R2, [R3, R2]		
-	BL VGA_write_char_ASM	
-	MOV R2, R4
+	//chech if input is valid
+	CMP R1, #60
+	BGE done
+	CMP R0, #80
+	BGE done
+	CMP R1, #0
+	BLT done
+	CMP R0, #0
+	BLT done
+
+	LDR R3, =ascii_chars		//load the ascii characters (from 0 to F) at the ascii_chars address (see bottom of the file)
+	MOV R4, R2					//store the byte 
+	LSR R2, R4, #4				//we get the first byte (4 most sig. bits)
+	LDRB R2, [R3, R2]			//we get the character we need to write at address (ascii chars + valueof half-byte)
+	BL	VGA_write_char_ASM		//write the obtained character on the screen
+	ADD R0, R0, #1				//increment x
+	AND R2, R4, #15				//we get the first 4 bits of the input byte
+	LDRB R2, [R3, R2]			//get character to write
+	BL VGA_write_char_ASM		//write character
+	MOV R2, R4					//restore R2 (callee save)
 	B done
 
 test_char:
@@ -75,15 +86,15 @@ test_char:
 	MOV R2, #0	//c=0
 	B loop_test_char
 loop_test_char:
-	CMP R1, #60
+	CMP R1, #60					//if rows are full we exit
 	BEQ done
-	CMP R0, #80
+	CMP R0, #80					//if row is full we change rows
 	ADDEQ R1, R1, #1
 	MOVEQ R0, #0
 	BEQ loop_test_char
-	BL VGA_write_char_ASM
-	ADD R2, R2, #1
-	ADD R0, R0, #1
+	BL VGA_write_char_ASM		//write the char at the current coordinates
+	ADD R2, R2, #1				//increment char value
+	ADD R0, R0, #1				//increment X
 	B loop_test_char
 
 test_pixel:
@@ -91,18 +102,18 @@ test_pixel:
 	MOV R0, #0	//x=0
 	MOV R1, #0	//y=0
 	MOV R2, #0	//c=0
-	MOV R12, #320
+	MOV R12, #0x140
 	B loop_test_pixel
 loop_test_pixel:
-	CMP R1, #239
-	BGT done
-	CMP R0, R12
+	CMP R1, #239			//check if all rows are full
+	BGT done				//if so exit
+	CMP R0, R12				//check if row is full, if so change rows
 	ADDGT R1, R1, #1
 	MOVGT R0, #0
 	BGT loop_test_pixel
-	BL VGA_draw_point_ASM
-	ADD R0, R0, #1
-	ADD R2, R2, #1
+	BL VGA_draw_point_ASM	//draw point at current location/color
+	ADD R0, R0, #1			//increment x
+	ADD R2, R2, #1			//increment color
 	B loop_test_pixel
 	
 /*DRAW PIXEL POINT
@@ -116,6 +127,7 @@ loop_test_pixel:
 	*/
 VGA_draw_point_ASM:
 	PUSH {R3-R12, LR}
+	//check if input is valid
 	CMP R1, #0xEF
 	BGT done
 	CMP R0, R12
@@ -125,11 +137,11 @@ VGA_draw_point_ASM:
 	CMP R0, #0
 	BLT done
 	
-	LSL R3, R1, #10
-	LSL R4, R0, #1
-	ADD R3, R3, R4
-	ADD R3, R3, #0xC8000000
-	STRH R2, [R3]
+	LSL R3, R1, #10		//shift y to get correct address format
+	LSL R4, R0, #1		//shift x to get correct address format
+	ADD R3, R3, R4		//x+y
+	ADD R3, R3, #0xC8000000	//add base offset to computed address
+	STRH R2, [R3]		//store halfword at address
 	B done
 
 
@@ -137,9 +149,9 @@ VGA_draw_point_ASM:
 	//R0:					R7:shifted x
 	//R1:					R8: current address to clear
 	//R2:					R9: 0(black)
-	//R3:					R10: y=240 flag
+	//R3:x & y flag			R10: y=240 flag
 	//R4:current y			R11: x=320 flag
-	//R5:current x			R12: x & y flag
+	//R5:current x			R12: 
 	//R6:shifted y / value to add
 	*/
 VGA_clear_pixelbuff_ASM:
@@ -149,32 +161,32 @@ VGA_clear_pixelbuff_ASM:
 	MOV R9, #0
 	MOV R10, #0
 	MOV R11, #0
-	MOV R12, #0
+	MOV R3, #0
 	B loop_cp
 loop_cp:
-	BL compare_yp
+	BL compare_yp			//set row/column full flags
 	BL compare_xp
-	AND R12, R10, R11
-	CMP R12, #1
-	BEQ done
-	CMP R4, #240
+	AND R3, R10, R11		//check if both flags are active
+	CMP R3, #1
+	BEQ done				//if so clear is done: exit
+	CMP R4, #240			//else if only y is done, change column
 	MOVEQ R4, #0
 	ADDEQ R5, R5, #1
-	BEQ loop_cp
-	BL clear_p
-	ADD R4, R4, #1
+	BEQ loop_cp				//we start the loop again if y was done
+	BL clear_p				//clear the current pixel
+	ADD R4, R4, #1			//increment y
 	B loop_cp
 clear_p:
 	PUSH {R3-R12, LR}
-	LSL R6, R4, #10
-	LSL R7, R5, #1
-	ADD R6, R6, R7
-	ADD R8, R6, #0xC8000000
-	STRH R9, [R8]
-	B done
+	LSL R6, R4, #10			//shift y to get correct address format
+	LSL R7, R5, #1			//shift x
+	ADD R6, R6, R7			//add x to the shifted y
+	ADD R8, R6, #0xC8000000		//add everything to the base address
+	STRH R9, [R8]			//store 0 at address
+	B done					//exit
 compare_xp:
 	PUSH {LR}
-	CMP R5, #320
+	CMP R5, R12
 	MOVGE R10, #1
 	POP {LR}
 	BX LR
@@ -184,7 +196,6 @@ compare_yp:
 	MOVGE R11, #1
 	POP {LR}
 	BX LR
-
 
 /*CLEAR CHAR BUFFER
 	//R0:					R7:shifted x
@@ -197,32 +208,31 @@ compare_yp:
 	*/
 VGA_clear_charbuff_ASM:
 	PUSH {R3-R12, LR}
-	MOV R4, #0
+	MOV R3, #0		//AND set to 0 initially
+	MOV R4, #0		//x and y are initialized at 0
 	MOV R5, #0
-	MOV R9, #0
+	MOV R9, #0		//#0 is black (clear)
 	B loop_cc
 loop_cc:
-	BL compare_yc
+	BL compare_yc			//set row/column full flags
 	BL compare_xc
-	AND R12, R10, R11
-	CMP R12, #1
-	BEQ done
-	CMP R4, #60
+	AND R3, R10, R11		//check if both flags are active
+	CMP R3, #1
+	BEQ done				//if so clear is done: exit
+	CMP R4, #60				//else if only y is done, change column
 	MOVEQ R4, #0
 	ADDEQ R5, R5, #1
-	BEQ loop_cc
-	BL clear_c
-	ADD R4, R4, #1
+	BEQ loop_cc				//we start the loop again if y was done
+	BL clear_c				//clear the current char
+	ADD R4, R4, #1			//increment y
 	B loop_cc
 clear_c:
 	PUSH {R3-R12, LR}
-	LSL R6, R4, #7
-	LSL R7, R5, #0
-	ADD R6, R6, R7
-	ADD R8, R6, #0xC9000000
-	STRB R9, [R8]
-	B done	
-
+	LSL R6, R4, #7			//shift y to get correct address format
+	ADD R6, R6, R5			//add x to the shifted y
+	ADD R8, R6, #0xC9000000		//add everything to the base address
+	STRB R9, [R8]			//store 0 at address
+	B done					//exit subroutine
 compare_xc:
 	PUSH {LR}
 	CMP R5, #80
@@ -236,7 +246,6 @@ compare_yc:
 	POP {LR}
 	BX LR
 
-
 /*WRITE CHAR BUFFER
 	//R0:X						R7:
 	//R1:Y						R8: 
@@ -248,6 +257,7 @@ compare_yc:
 	*/
 VGA_write_char_ASM:
 	PUSH {R3-R12, LR}
+	//check if input is valid
 	CMP R1, #60
 	BGE done
 	CMP R0, #80
@@ -257,11 +267,11 @@ VGA_write_char_ASM:
 	CMP R0, #0
 	BLT done
 	
-	LSL R3, R1, #7
-	ADD R3, R3, R0
-	ADD R4, R3, #0xC9000000
-	STRB R2, [R4]
-	B done
+	LSL R3, R1, #7		//shift y by 7 bytes to get correct address format
+	ADD R3, R3, R0		//add the x coordinate
+	ADD R4, R3, #0xC9000000		//add x+y to teh base address of the buffer
+	STRB R2, [R4]		//write the char value at the address
+	B done		//exit
 
 done:
 	POP {R3-R12, LR}
--- Lab4/ps2_keyboard.s
+++ Lab4/ps2_keyboard.s
@@ -1,70 +1,82 @@
-//ps2 data 0xFF200100
-//RVALID bit 15
-
-.equ PS_2, 0xFF200100
+.equ PS_2, 0xFF200100	//address of the ps2 register
 .global _start
 
 _start:
 
+//initialization of the function: clearing the buffers
 init:
 	BL VGA_clear_charbuff_ASM
 	BL VGA_clear_pixelbuff_ASM
-	MOV R3, #0
-	MOV R4, #0
+	MOV R3, #0		//set x=0
+	MOV R4, #0		//set y=0
 	B main
 
 //R3 = X
 //R4 = Y
 main:
-	LDR R0, =char
-	BL read_PS2_data_ASM
-	CMP R0, #1
-	BNE main
-	CMP R3, #80
-	ADDGE R4, R4, #1
+	LDR R0, =char					//R0 holds the adress at which we want to copy the keyboard data byte
+	BL read_PS2_data_ASM			//call read_data with R0 as input
+	CMP R0, #1						//check if data was valid
+	BNE main						//if not valid read again
+	CMP R3, #80						//if valid check if the row is filled
+	ADDGE R4, R4, #1				//if yes change rows
 	MOVGE R3, #0
-	CMP R4, #60
-	BEQ init
-	MOV R0, R3
+	CMP R4, #60						//check if the screen is filled
+	BEQ init						//if so restart the program
+	MOV R0, R3						//move the coordinates and char inputs to the right registers
 	MOV R1, R4	
 	LDRB R2, char
-	BL VGA_write_byte_ASM
-	ADD R3, R3, #3
+	BL VGA_write_byte_ASM			//write the byte to the screen 
+	ADD R3, R3, #3					//increment coordinates
 	B main
+
+	//reads data from keyboard and chechs if valid
 
 read_PS2_data_ASM:
 	PUSH {R3-R12, LR}
-	LDR R2, =PS_2
-	LDR R3, [R2]
-	AND R4, R3, #0x8000
-	CMP R4, #0
+	LDR R2, =PS_2			//R2 holds the ps2 data address
+	LDR R3, [R2]			//R3 holds the value at this adress (word)
+	AND R4, R3, #0x8000		//we get bit 15 (RVALID)
+	CMP R4, #0				//if VALID is 0, we return 0 and exit
 	MOVEQ R0, #0		
 	BEQ done
 
 	//data is valid
-	AND R4, R3, #0xFF
-	MOV R2, R0
+	AND R4, R3, #0xFF		//if data is valid get the first byte
+	MOV R2, R0				//store the byte in the char address
 	STRB R4, [R2]
-	MOV R0, #1
+	MOV R0, #1				//return 1
 	B done
 
-
+//writes an input byte (R2) at the given coordinates (R0, R1)
 VGA_write_byte_ASM:
 	PUSH {R3-R12, LR}
-	LDR R3, =ascii_chars	
-	MOV R4, R2				
-	LSR R2, R4, #4			
-	LDRB R2, [R3, R2]		
-	BL	VGA_write_char_ASM	
-	ADD R0, R0, #1			
-	AND R2, R4, #15			
-	LDRB R2, [R3, R2]		
-	BL VGA_write_char_ASM	
-	MOV R2, R4
+	//chech if input is valid
+	CMP R1, #60
+	BGE done
+	CMP R0, #80
+	BGE done
+	CMP R1, #0
+	BLT done
+	CMP R0, #0
+	BLT done
+
+	LDR R3, =ascii_chars		//load the ascii characters (from 0 to F) at the ascii_chars address (see bottom of the file)
+	MOV R4, R2					//store the byte 
+	LSR R2, R4, #4				//we get the first byte (4 most sig. bits)
+	LDRB R2, [R3, R2]			//we get the character we need to write at address (ascii chars + valueof half-byte)
+	BL	VGA_write_char_ASM		//write the obtained character on the screen
+	ADD R0, R0, #1				//increment x
+	AND R2, R4, #15				//we get the first 4 bits of the input byte
+	LDRB R2, [R3, R2]			//get character to write
+	BL VGA_write_char_ASM		//write character
+	MOV R2, R4					//restore R2 (callee save)
 	B done
 
+//writes an input char at the given coordinates
 VGA_write_char_ASM:
 	PUSH {R3-R12, LR}
+	//check if input is valid
 	CMP R1, #60
 	BGE done
 	CMP R0, #80
@@ -74,48 +86,39 @@ VGA_write_char_ASM:
 	CMP R0, #0
 	BLT done
 	
-	LSL R3, R1, #7
-	ADD R3, R3, R0
-	ADD R4, R3, #0xC9000000
-	STRB R2, [R4]
-	B done
-
-done:
-	POP {R3-R12, LR}
-	BX LR
-
-ascii_chars:
-	.ascii "0123456789ABCDEF"
-char:
-	.space 4
+	LSL R3, R1, #7		//shift y by 7 bytes to get correct address format
+	ADD R3, R3, R0		//add the x coordinate
+	ADD R4, R3, #0xC9000000		//add x+y to teh base address of the buffer
+	STRB R2, [R4]		//write the char value at the address
+	B done		//exit
 
 VGA_clear_charbuff_ASM:
 	PUSH {R3-R12, LR}
-	MOV R4, #0
+	MOV R3, #0		//AND set to 0 initially
+	MOV R4, #0		//x and y are initialized at 0
 	MOV R5, #0
-	MOV R9, #0
+	MOV R9, #0		//#0 is black (clear)
 	B loop_cc
 loop_cc:
-	BL compare_yc
+	BL compare_yc			//set row/column full flags
 	BL compare_xc
-	AND R12, R10, R11
-	CMP R12, #1
-	BEQ done
-	CMP R4, #60
+	AND R3, R10, R11		//check if both flags are active
+	CMP R3, #1
+	BEQ done				//if so clear is done: exit
+	CMP R4, #60				//else if only y is done, change column
 	MOVEQ R4, #0
 	ADDEQ R5, R5, #1
-	BEQ loop_cc
-	BL clear_c
-	ADD R4, R4, #1
+	BEQ loop_cc				//we start the loop again if y was done
+	BL clear_c				//clear the current char
+	ADD R4, R4, #1			//increment y
 	B loop_cc
 clear_c:
 	PUSH {R3-R12, LR}
-	LSL R6, R4, #7
-	LSL R7, R5, #0
-	ADD R6, R6, R7
-	ADD R8, R6, #0xC9000000
-	STRB R9, [R8]
-	B done	
+	LSL R6, R4, #7			//shift y to get correct address format
+	ADD R6, R6, R5			//add x to the shifted y
+	ADD R8, R6, #0xC9000000		//add everything to the base address
+	STRB R9, [R8]			//store 0 at address
+	B done					//exit subroutine
 
 compare_xc:
 	PUSH {LR}
@@ -137,32 +140,32 @@ VGA_clear_pixelbuff_ASM:
 	MOV R9, #0
 	MOV R10, #0
 	MOV R11, #0
-	MOV R12, #0
+	MOV R3, #0
 	B loop_cp
 loop_cp:
-	BL compare_yp
+	BL compare_yp			//set row/column full flags
 	BL compare_xp
-	AND R12, R10, R11
-	CMP R12, #1
-	BEQ done
-	CMP R4, #240
+	AND R3, R10, R11		//check if both flags are active
+	CMP R3, #1
+	BEQ done				//if so clear is done: exit
+	CMP R4, #240			//else if only y is done, change column
 	MOVEQ R4, #0
 	ADDEQ R5, R5, #1
-	BEQ loop_cp
-	BL clear_p
-	ADD R4, R4, #1
+	BEQ loop_cp				//we start the loop again if y was done
+	BL clear_p				//clear the current pixel
+	ADD R4, R4, #1			//increment y
 	B loop_cp
 clear_p:
 	PUSH {R3-R12, LR}
-	LSL R6, R4, #10
-	LSL R7, R5, #1
-	ADD R6, R6, R7
-	ADD R8, R6, #0xC8000000
-	STRH R9, [R8]
-	B done
+	LSL R6, R4, #10			//shift y to get correct address format
+	LSL R7, R5, #1			//shift x
+	ADD R6, R6, R7			//add x to the shifted y
+	ADD R8, R6, #0xC8000000		//add everything to the base address
+	STRH R9, [R8]			//store 0 at address
+	B done					//exit
 compare_xp:
 	PUSH {LR}
-	CMP R5, #320
+	CMP R5, R12
 	MOVGE R10, #1
 	POP {LR}
 	BX LR
@@ -173,4 +176,14 @@ compare_yp:
 	POP {LR}
 	BX LR
 
+done:
+	POP {R3-R12, LR}
+	BX LR
+
+ascii_chars:
+	.ascii "0123456789ABCDEF"
+
+//space saved to store keyboard data
+char:
+	.space 4
 	.end
